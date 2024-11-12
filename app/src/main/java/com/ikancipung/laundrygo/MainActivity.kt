3ea3c479c0d1909db54b1627946ef591762774d1
sleepy <idhammakbar@gmai.com>
2024-11-12 13:49:16 +0700

    [testing]

--- app/src/main/java/com/ikancipung/laundrygo/MainActivity.kt
+++ app/src/main/java/com/ikancipung/laundrygo/MainActivity.kt
@@ -20,10 +20,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             LaundryGOTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android test",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
