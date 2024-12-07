c5bd20e853b7bad578d5b9b6d43271d98dc9895e
myuscasucc <mcpc.mvp@gmail.com>
2024-12-07 11:14:50 +0700

    Added Firebase adn Navigation

--- app/src/main/java/com/ikancipung/laundrygo/MainActivity.kt
+++ app/src/main/java/com/ikancipung/laundrygo/MainActivity.kt
@@ -3,16 +3,49 @@ package com.ikancipung.laundrygo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.ikancipung.laundrygo.login.LoginScreen
+import com.ikancipung.laundrygo.menu.Homepage
+import com.ikancipung.laundrygo.order.LaundryOrderScreen
+import com.ikancipung.laundrygo.order.RatingScreen
+import com.ikancipung.laundrygo.order.TitleLaundryPreview
 import com.ikancipung.laundrygo.order.TitleLaundryScreen
+import com.ikancipung.laundrygo.order.myOrder
+import com.ikancipung.laundrygo.payment.PaymentScreen
+import com.ikancipung.laundrygo.profile.Profile
+import com.ikancipung.laundrygo.profile.ProfileLaundry
+import com.ikancipung.laundrygo.signup.SignUpScreen
 import com.ikancipung.laundrygo.ui.theme.LaundryGOTheme
 
+private lateinit var auth: FirebaseAuth
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            auth = Firebase.auth
+            val currentUser = auth.currentUser
+            val startDestination = if (currentUser != null) "Home" else "Login"
+
+            val navController = rememberNavController()
+
+
             LaundryGOTheme {
-                // Memanggil TitleLaundryScreen dengan contoh orderId
-                TitleLaundryScreen(orderId = "L-93V832NM102")
+                NavHost(navController, startDestination = startDestination){
+                    composable("Login"){LoginScreen(navController=navController)
+                    }
+                    composable("Signup") {
+                        SignUpScreen(navController = navController)
+                    }
+                    composable("Homepage") {
+                        Homepage(navController = navController)
+                    }
+                }
             }
         }
     }
