6d7bc3677dfcadfdf08310a1d36428a0f1ce2201
myuscasucc <mcpc.mvp@gmail.com>
2024-12-07 11:14:13 +0700

    Removed Classes and Changed to Method

--- app/src/main/java/com/ikancipung/laundrygo/order/RatingPage.kt
+++ app/src/main/java/com/ikancipung/laundrygo/order/RatingPage.kt
@@ -1,5 +1,6 @@
 package com.ikancipung.laundrygo.order
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -32,193 +33,194 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ikancipung.laundrygo.R
 import com.ikancipung.laundrygo.ui.theme.BlueLaundryGo
 
-class RatingPage {
-    @Composable
-    fun RatingScreen() {
-        var rating by remember { mutableStateOf(1f) }
+@Composable
+fun RatingScreen() {
+    var rating by remember { mutableStateOf(1f) }
 
-        Column(
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Header Section
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxWidth()
+                .padding(bottom = 8.dp)
         ) {
-            // Header Section
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
+            // Back Icon
+            Image(
+                painter = painterResource(R.drawable.keluar),
+                contentDescription = "Copy",
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp)
-            ) {
-                // Back Icon
-                IconButton(onClick = { /* Handle back navigation */ }) {
-                    Icon(
-                        imageVector = Icons.Default.ArrowBack,
-                        contentDescription = "Back"
-                    )
-                }
-
-                // Title and Location
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .weight(1f) // Center the column evenly within the row
-                ) {
-                    Text(
-                        text = "Loandry",
-                        style = MaterialTheme.typography.bodyMedium,
-                        textAlign = TextAlign.Center
-                    )
-                    Text(
-                        text = "Klojen, Malang",
-                        style = MaterialTheme.typography.bodySmall,
-                        color = Color.Gray,
-                        textAlign = TextAlign.Center
-                    )
-                }
-
-                // Add a Spacer for alignment
-                Spacer(modifier = Modifier.width(48.dp)) // Matches the size of IconButton
-            }
-            // Order Details Section
-            Spacer(modifier = Modifier.height(32.dp))
-            Column(
-                modifier = Modifier.fillMaxWidth().padding(16.dp)
-            ) {
-                InfoRow(label = "Waktu Pemesanan", value = "14 Oct 2024, 09:39")
-                InfoRow(label = "Waktu Selesai", value = "15 Oct 2024, 10:11")
-                InfoRow(label = "Order ID", value = "L-93V832NM102")
-                InfoRow(label = "Metode Pembayaran", value = "QRIS")
-                InfoRow(label = "Biaya", value = "Rp. 80.000")
-            }
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            // Rating Section
-            Row(){
-                Text(text = "Menyukai Laundry ini? ",
-                    style = MaterialTheme.typography.bodyMedium)
-                Text(
-                    "Tambahkan ke favorit!",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = Color.Blue,
-                    modifier = Modifier.clickable { /* Handle favorite action */ }
-                )
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                StarRatingBar(
-                    maxStars = 5,
-                    rating = rating,
-                    onRatingChanged = { rating = it }
-                )
-            }
-
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "Dengan memberikan rating, anda dapat membantu meningkatkan pelayanan LaundryGO",
-                style = MaterialTheme.typography.bodySmall,
-                fontSize = 12.sp,
-                textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                    .size(24.dp) // Set the size of the image
+                    .clickable{ } // Add clickable functionality
             )
 
-            Spacer(modifier = Modifier.height(24.dp))
+            // Title and Location
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .weight(1f) // Center the column evenly within the row
+            ) {
+                Text(
+                    text = "Loandry",
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "Klojen, Malang",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.Gray,
+                    textAlign = TextAlign.Center
+                )
+            }
 
+            // Add a Spacer for alignment
+            Spacer(modifier = Modifier.width(48.dp)) // Matches the size of IconButton
+        }
+        // Order Details Section
+        Spacer(modifier = Modifier.height(32.dp))
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
+        ) {
+            InfoRow(label = "Waktu Pemesanan", value = "14 Oct 2024, 09:39")
+            InfoRow(label = "Waktu Selesai", value = "15 Oct 2024, 10:11")
+            InfoRow(label = "Order ID", value = "L-93V832NM102")
+            InfoRow(label = "Metode Pembayaran", value = "QRIS")
+            InfoRow(label = "Biaya", value = "Rp. 80.000")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Rating Section
+        Row(){
+            Text(text = "Menyukai Laundry ini? ",
+                style = MaterialTheme.typography.bodyMedium)
             Text(
-                text = "Laporkan Kendala!",
+                "Tambahkan ke favorit!",
                 style = MaterialTheme.typography.bodyMedium,
                 color = Color.Blue,
-                modifier = Modifier.clickable { /* Handle report issue */ }
+                modifier = Modifier.clickable { /* Handle favorite action */ }
             )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Button(
-                onClick = {
-                    // Handle submit action
-                },
-                modifier = Modifier.fillMaxWidth(),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
-            ) {
-                Text(text = "Submit", color = Color.White)
-            }
         }
-    }
 
-
-    @Composable
-    fun InfoRow(label: String, value: String) {
+        Spacer(modifier = Modifier.height(16.dp))
         Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            StarRatingBar(
+                maxStars = 5,
+                rating = rating,
+                onRatingChanged = { rating = it }
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Dengan memberikan rating, anda dapat membantu meningkatkan pelayanan LaundryGO",
+            style = MaterialTheme.typography.bodySmall,
+            fontSize = 12.sp,
+            textAlign = androidx.compose.ui.text.style.TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Laporkan Kendala!",
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Blue,
+            modifier = Modifier.clickable { /* Handle report issue */ }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                // Handle submit action
+            },
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
         ) {
-            Text(
-                text = label,
-                style = MaterialTheme.typography.bodyMedium,
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = value,
-                style = MaterialTheme.typography.bodyMedium
-            )
+            Text(text = "Submit", color = Color.White)
         }
     }
+}
 
-    //Credit to imitiyaz125
-    @Composable
-    fun StarRatingBar(
-        maxStars: Int = 5,
-        rating: Float,
-        onRatingChanged: (Float) -> Unit
+
+@Composable
+fun InfoRow(label: String, value: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        val density = LocalDensity.current.density
-        val starSize = (12f * density).dp
-        val starSpacing = (0.5f * density).dp
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = value,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
 
-        Row(
-            modifier = Modifier.selectableGroup(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            for (i in 1..maxStars) {
-                val isSelected = i <= rating
-                val icon = if (isSelected) Icons.Filled.Star else Icons.Default.Star
-                val iconTintColor = if (isSelected) BlueLaundryGo else Color(0xF0E0E0E0)
-                Icon(
-                    imageVector = icon,
-                    contentDescription = null,
-                    tint = iconTintColor,
-                    modifier = Modifier
-                        .selectable(
-                            selected = isSelected,
-                            onClick = {
-                                onRatingChanged(i.toFloat())
-                            }
-                        )
-                        .width(starSize).height(starSize)
-                )
+//Credit to imitiyaz125
+@Composable
+fun StarRatingBar(
+    maxStars: Int = 5,
+    rating: Float,
+    onRatingChanged: (Float) -> Unit
+) {
+    val density = LocalDensity.current.density
+    val starSize = (12f * density).dp
+    val starSpacing = (0.5f * density).dp
 
-                if (i < maxStars) {
-                    Spacer(modifier = Modifier.width(starSpacing))
-                }
+    Row(
+        modifier = Modifier.selectableGroup(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        for (i in 1..maxStars) {
+            val isSelected = i <= rating
+            val icon = if (isSelected) Icons.Filled.Star else Icons.Default.Star
+            val iconTintColor = if (isSelected) BlueLaundryGo else Color(0xF0E0E0E0)
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = iconTintColor,
+                modifier = Modifier
+                    .selectable(
+                        selected = isSelected,
+                        onClick = {
+                            onRatingChanged(i.toFloat())
+                        }
+                    )
+                    .width(starSize).height(starSize)
+            )
+
+            if (i < maxStars) {
+                Spacer(modifier = Modifier.width(starSpacing))
             }
         }
     }
+}
 
-    @Preview(showBackground = true)
-    @Composable
-    fun PrevLaundryOrder() {
-        RatingScreen()
-    }
+@Preview(showBackground = true)
+@Composable
+fun PrevLaundryScreen() {
+    RatingScreen()
 }
