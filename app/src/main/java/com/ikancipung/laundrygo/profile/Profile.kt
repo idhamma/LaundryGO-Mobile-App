5bac9a811cc1a9c7b37d6253266f91095d80bee5
rakhaalif <xpn9x123@gmail.com>
2024-12-07 14:11:56 +0700

    dzakwan fix

--- app/src/main/java/com/ikancipung/laundrygo/profile/Profile.kt
+++ app/src/main/java/com/ikancipung/laundrygo/profile/Profile.kt
@@ -3,14 +3,8 @@ package com.ikancipung.laundrygo.profile
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
@@ -30,7 +24,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.ikancipung.laundrygo.R
@@ -49,31 +42,39 @@ fun Profile(navController: NavController) {
     email = "boscipung@gmail.com"
     address = "Jl. Green Andara Residences Blok B3 No. 19, Pangkalan Jati"
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Profil",
-            modifier = Modifier.padding(vertical = 8.dp),
-            style = MaterialTheme.typography.titleLarge,
-            fontWeight = FontWeight(500)
-        )
+        item {
+            Text(
+                text = "Profil",
+                modifier = Modifier.padding(vertical = 8.dp),
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight(500)
+            )
+        }
 
-        PhotoProfile()
+        item {
+            PhotoProfile()
+        }
 
-        Text(
-            text = username,
-            modifier = Modifier.padding(vertical = 8.dp),
-            style = MaterialTheme.typography.bodyLarge,
-            fontWeight = FontWeight(500)
-        )
+        item {
+            Text(
+                text = username,
+                modifier = Modifier.padding(vertical = 8.dp),
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight(500)
+            )
+        }
 
-        DataProfile(
-            username = username, phoneNumber = phoneNumber, email = email, address = address
-        )
+        item {
+            DataProfile(
+                username = username, phoneNumber = phoneNumber, email = email, address = address
+            )
+        }
     }
 }
 
@@ -182,9 +183,3 @@ fun FieldDataProfile(
         modifier = Modifier.padding(bottom = 16.dp), thickness = 0.25.dp, color = Color.Gray
     )
 }
-
-//@Preview
-//@Composable
-//fun PrevProfile() {
-//    Profile()
-//}
